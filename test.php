67d0735ab84350cf5368dcdba59d9ae91a029dd7
zhujunya <zhujunya@jingdata.com>
2018-06-19 11:04:09 +0800

    Testing

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BJQCDN0808
+ * Date: 2018/6/19
+ * Time: 上午10:48
+ */
+
+class test
+{
+    public function test(){
+        if(true){
+            echo 'nihao';
+        }
+    }
+}
