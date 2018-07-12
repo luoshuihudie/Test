e28f6579690afdd26ca9bb735a0d1329e6000fb0
zhujunya <zhujunya@jingdata.com>
2018-07-12 16:00:40 +0800

    Test Fixer2

--- Test/app/Http/Controllers/TestController.php
+++ Test/app/Http/Controllers/TestController.php
@@ -1,52 +1,91 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Models\Company;
 
-use Illuminate\Http\Request;
-use App\Jobs\TagLabel;
-use Illuminate\Support\Facades\Redis;
+use Illuminate\Database\Eloquent\Model;
 
-class TestController extends Controller
+class MediaReport extends Model
 {
-    //
-    public function test($uid = 0, $score = 0)
+    protected $connection = 'crm';
+    protected $table      = 'company_media_reports';
+    protected $primaryKey = 'id';
+    protected $guarded    = ['id'];
+    protected $hidden =['content'];
+    public $timestamps    = false;
+
+    public function companies()
     {
-        $data['uid'] = $uid;
-        $data['score'] = $score;
-        $date = date('Y-m-d', time());
-//        $redisKey = 'pintu:scoredevice-' . $date;
-        $redisKey = 'pintu:scoredevice-1';
-        dump(Redis::hgetall($redisKey));
-        $result = Redis::hget($redisKey, $data['uid']);
-        dump($result);
-        if ($result) {
-            return 0;
-        }
-
-        dump(Redis::exists($redisKey));
-        dump(Redis::hget($redisKey, '530'));
-        if (!Redis::exists($redisKey)) {
-            Redis::hset($redisKey, $data['uid'], $data['score']);
-            dump('Test1');
-            Redis::expireAt($redisKey, time()+500);
-        } else {
-            Redis::hset($redisKey, $data['uid'], $data['score']);
-            dump('Test2');
-        }
-        if (true) {
-            Redis::hset($redisKey, $data['uid'], $data['score']);
-        }
-//        echo $redisKey;
-        dump(Redis::exists($redisKey));
-        dump(Redis::KEYS('*'));
-
-//        return Redis::get('key');
-//        $job = (new TagLabel())->onConnection('redis')->onQueue('pintu:tag-label');
-//        $this->dispatch($job);
-//        $this->dispatch(new TagLabel());
+        return $this->belongsToMany(\App\Models\Company::class, 'company_report_relation', 'report_id', 'cid')
+            ->withPivot('type', 'type_extend', 'is_rong');
     }
 
-    public function test1(){
-        echo "fffff";
+    public function investments()
+    {
+        return $this->belongsToMany(\App\Models\Investment::class, 'investment_report_relation', 'report_id', 'investment_id');
+    }
+
+    public function companyRelations()
+    {
+        return $this->hasMany(\App\Models\Company\ReportRelation::class, 'report_id', 'id');
+    }
+
+    public function investmentRelations()
+    {
+        return $this->hasMany(\App\Models\Investment\ReportRelation::class, 'report_id', 'id');
+    }
+
+    public function tagRelations()
+    {
+        return $this->hasMany(\App\Models\Tag\ReportRelation::class, 'report_id', 'id');
+    }
+
+    public function tagPolicyRelations()
+    {
+        return $this->hasMany(\App\Models\Tag\PolicyReportRelation::class, 'report_id', 'id');
+    }
+
+    public function investorRelations_kkk()
+    {
+        return $this->hasMany(\App\Models\Investor\ReportRelation::class, 'report_id', 'id');
+    }
+
+    public function orgRelations()
+    {
+        return $this->hasMany(\App\Models\Organization\ReportRelation::class, 'report_id', 'id');
+    }
+
+
+    public function serviceOrgReportRelation()
+    {
+        return $this->hasMany(\JingData\Models\Crm\ServiceOrgReportRelation::class, 'report_id', 'id');
+    }
+
+    public function logs()
+    {
+        return $this->hasMany(\App\Models\OperateLog::class, 'rid', 'id')->where('type', 'ffffffffffffffffffffffffffffbasic.media.operation');
+    }
+
+    public function autoCompany()
+    {
+        return $this->hasOne(\App\Models\Company::class, 'id', 'auto_cid');
+    }
+
+    function auditor()
+    {
+        return $this->hasOne(\App\Models\System\User::class, 'id', 'uid');
+    }
+
+    function personReportRelation_hjjj()
+    {
+        return $this->hasMany(\App\Models\Person\PersonReportRelation::class, 'report_id', 'id');
+    }
+
+    public function lpRelations_fff()
+    {
+        return $this->hasMany(\App\Models\Lp\LpReportRelation::class,  'report_id', 'id');
+    }
+    public function lps_ggg()
+    {
+        return $this->belongsToMany(\App\Models\Lp::class, 'lp_report_relation', 'id', 'lp_id');
     }
 }
